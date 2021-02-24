ccdf27a8b57d88a638d98593906ea6a5d3a5449b
Abdi Abdulle <abdi.abdulle@outlook.dk>
2021-02-24 10:26:37 +0100

    update task 1

--- exercises_1/src/main/java/Main.java
+++ exercises_1/src/main/java/Main.java
@@ -1,22 +1,18 @@
 
 
 class MyRunnable implements Runnable{
-    private int x;
-    private int y;
 
-    public MyRunnable(int x, int y){
-        this.x = x;
-        this.y = y;
+    private long sum;
+
+    public MyRunnable(){
+
     }
     public void run() {
-        int sum = x + y;
-        System.out.println(sum);
 
         for (int i = 0; i < 1000000000; i++) {
-            int res = sum + i;
-            i++;
-            System.out.println(res);
+           sum += i;
         }
+        System.out.println(sum);
         }
 }
 public class Main {
@@ -27,14 +23,14 @@ public class Main {
         Runnable r2 = ()-> {System.out.println("Mo");};
         Thread t = new Thread(r);
         Thread t2 = new Thread(r2);
-        Thread t3 = new Thread(new MyRunnable(20, 10));
+        Thread t3 = new Thread(new MyRunnable());
         Thread task2 = new Thread(new MyRunnable2());
         Thread task3 = new Thread(new MyRunnable3());
        // t.start();
        // t2.start();
-       // t3.start();
+        t3.start();
        // task2.start();
-        task3.start();
+        //task3.start();
 
     }
 }
